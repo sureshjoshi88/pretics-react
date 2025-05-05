bc75e780b7f87ac967bd115316f5214b907d11eb
sureshjoshi88 <sj3795789@gmail.com>
2025-05-05 21:36:25 +0530

    updated code

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -27,14 +27,14 @@ const Card = () => {
       <div>
         <img className='w-50' src="https://m.media-amazon.com/images/I/71Sp7BKB0CL.jpg" alt="balaji-img" />
       </div>
-      <div className='flex justify-between mt-10'>
-        <button onClick={counter} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>Like <i class="fa-solid fa-thumbs-up" style="color: #0d0d0d;"></i></button>
-        <button onClick={counter2} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>Dislike <i class="fa-solid fa-thumbs-down" style="color: #121212;"></i></button>
+      <div className='flex justify-between mt-10 gap-5'>
+        <button onClick={counter} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>Like <i className="fa-solid fa-thumbs-up" style={{color: "#0d0d0d;"}}></i></button>
+        <button onClick={counter2} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>Dislike <i className="fa-solid fa-thumbs-down" style={{color: "#0d0d0d;"}}></i></button>
         <button onClick={reset} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>reset</button>
       </div>
-      <div className='flex gap-4'>
-        <button className='w-10 ' id='buton-1'>0</button>
-        <button className='w-10 ' id='buton-2'>0</button>
+      <div className='flex gap-14'>
+        <button className='w-10 text-2xl' id='buton-1'>0</button>
+        <button className='w-10 text-2xl' id='buton-2'>0</button>
       </div>
       </div>
     </div>
