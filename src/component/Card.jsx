fbcc56c263bc28fa2bb18afe6621b5a85fd4dbf8
sureshjoshi88 <sj3795789@gmail.com>
2025-05-06 14:09:44 +0530

    add new react-icons

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,5 +1,7 @@
 import {React} from 'react'
 import { useState } from 'react';
+import { AiFillLike,AiFillDislike  } from "react-icons/ai";
+
 
 const Card = (props) => {
 
@@ -38,17 +40,22 @@ const Card = (props) => {
       <p>{props.title}</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quaerat quisquam cumque, nemo debitis autem cum architecto inventore vitae suscipit.</p>
       <div className='flex justify-between mt-3 gap-5 w-80'>
-        <button onClick={counter} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>Like <i className="fa-solid fa-thumbs-up" style={{color: "#0d0d0d;"}}></i></button>
-        <button onClick={counter2} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>Dislike <i className="fa-solid fa-thumbs-down" style={{color: "#0d0d0d;"}}></i></button>
+        {/* <button onClick={counter} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>Like <i className="fa-solid fa-thumbs-up" style={{color: "#0d0d0d;"}}></i></button> */}
+        <div  onClick={counter} className="text-5xl text-green-700">
+          <AiFillLike />
+        </div>
+          <div onClick={counter2} className="text-5xl text-red-700">
+            <AiFillDislike />
+          </div>
         <button onClick={reset} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>reset</button>
       </div>
       <div className='flex gap-14'>
         <button className='w-10 text-2xl' id='buton-1'>{like}</button>
         <button className='w-10 text-2xl' id='buton-2'>{dislike}</button>
       </div>
-      <div>
-        <input className="border outline-0" id="input" type="text" placeholder="Enter a comment" />
-        <button onClick={button} className="border">sumbit</button>
+      <div className="flex gap-2">
+        <input className="border outline-0 rounded" id="input" type="text" placeholder="Enter a comment" />
+        <button onClick={button} className="border rounded  'bg-sky-300 p-1  font-bold hover:bg-sky-500">sumbit</button>
       </div>
       <div>
         {comment.map((value)=>
