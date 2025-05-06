4db34d49a14ce7d7ffc007894f81011866dfdfc6
sureshjoshi88 <sj3795789@gmail.com>
2025-05-06 12:08:06 +0530

    update

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -6,6 +6,16 @@ const Card = (props) => {
 
     const [like,setLike] = useState(0);
     const [dislike,setDislike] = useState(0);
+    const [comment,setComment] = useState([]);
+
+    const button=()=>{
+      let input = document.getElementById("input");
+      console.log(comment);
+      setComment([...comment,input.value]);
+      input.value = ""
+      
+      
+    }
 
     const counter=()=>{
        
@@ -27,7 +37,7 @@ const Card = (props) => {
       <div className="bg-white rounded shadow-xl p-2 mt-3 border border-amber-600">
       <p>{props.title}</p>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quaerat quisquam cumque, nemo debitis autem cum architecto inventore vitae suscipit.</p>
-      <div className='flex justify-between mt-3 gap-5'>
+      <div className='flex justify-between mt-3 gap-5 w-80'>
         <button onClick={counter} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>Like <i className="fa-solid fa-thumbs-up" style={{color: "#0d0d0d;"}}></i></button>
         <button onClick={counter2} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>Dislike <i className="fa-solid fa-thumbs-down" style={{color: "#0d0d0d;"}}></i></button>
         <button onClick={reset} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>reset</button>
@@ -35,6 +45,15 @@ const Card = (props) => {
       <div className='flex gap-14'>
         <button className='w-10 text-2xl' id='buton-1'>{like}</button>
         <button className='w-10 text-2xl' id='buton-2'>{dislike}</button>
+      </div>
+      <div>
+        <input className="border outline-0" id="input" type="text" placeholder="Enter a comment" />
+        <button onClick={button} className="border">sumbit</button>
+      </div>
+      <div>
+        {comment.map((value)=>
+        <p>{value}</p>
+        )}
       </div>
       </div>
      </div>
--- src/component/Card2.jsx
+++ src/component/Card2.jsx
@@ -20,12 +20,12 @@ const Card2 = (props) => {
 
       }
     return (
-      <div className='flex justify-center'>
+      <div className='flex justify-center mt-4'>
        <div>
         <div className="bg-white rounded shadow-xl p-2 mt-3 border border-amber-600">
         <p>{props.title}</p>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur quaerat quisquam cumque, nemo debitis autem cum architecto inventore vitae suscipit.</p>
-        <div className='flex justify-between mt-3 gap-5'>
+        <div className='flex justify-between mt-3 gap-5 w-80'>
           <button onClick={counter} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>Like <i className="fa-solid fa-thumbs-up" style={{color: "#0d0d0d;"}}></i></button>
           <button onClick={counter2} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>Dislike <i className="fa-solid fa-thumbs-down" style={{color: "#0d0d0d;"}}></i></button>
           <button onClick={reset} className='bg-sky-300 p-2 rounded font-bold hover:bg-sky-500'>reset</button>
