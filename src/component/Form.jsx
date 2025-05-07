fe7f106e5100b07d1d2782b05714e0fd3d9cc56a
sureshjoshi88 <sj3795789@gmail.com>
2025-05-07 16:50:46 +0530

    updated code

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
 const Form = () => {
+  function mainFull(){
+    let main = document.getElementById("main")
+    main.addEventListener("submit",(e)=>[
+        e.preventDefault()
+    ])
+  }
     return (
         <div className='flex justify-center mt-10'>
             <div className=' p-2 rounded shadow-xl bg-white'>
-                <form action="">
+                <form action="" id='main' onSubmit={mainFull}>
                     <div>
                         <input className='border mt-3 w-100 p-1 rounded' type="email" placeholder='Username' required />
                     </div>
@@ -12,7 +18,7 @@ const Form = () => {
                         <input className='border mt-3 w-100 p-1 rounded' type="password" placeholder='Password' required />
                     </div>
                     <div>
-                        <button className='p-1 bg-green-500  rounded w-100 mt-3'>Sumbit</button>
+                        <button className='p-1 bg-green-400 font-semibold  rounded w-100 mt-3'>Sumbit</button>
                     </div>
                 </form>
             </div>
