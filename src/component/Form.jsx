56603aeebb08ceff0421c5120de853cdbc9b54ba
sureshjoshi88 <sj3795789@gmail.com>
2025-05-08 11:15:34 +0530

    add new function and add tostyfy and set inputs

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -1,24 +1,76 @@
 import React from 'react'
+import { useState } from 'react';
+import Toastify from 'toastify-js'
+import "toastify-js/src/toastify.css"
+
+
 
 const Form = () => {
-  function mainFull(){
-    let main = document.getElementById("main")
-    main.addEventListener("submit",(e)=>[
-        e.preventDefault()
-    ])
-  }
+//   function mainFull(){
+//     let main = document.getElementById("main")
+//     main.addEventListener("submit",(e)=>[
+//         e.preventDefault()
+//     ])
+//   }
+const mainFull=(event)=>{
+event.preventDefault();
+}
+const [value,setValue] = useState("");
+const [value2,setValue2] = useState("");
+const values = {
+    username:"suresh@gmail.com",
+    password:"joshi123"
+}
+
+const sumbitButton=()=>{
+    if(values.username===value && values.password===value2){
+        setValue("")
+        setValue2("")
+        Toastify({
+            text: "Welcome user you are login",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "linear-gradient(to right, #00b09b, #96c93d)",
+            },
+            onClick: function(){} // Callback after click
+          }).showToast();
+    }else{
+        Toastify({
+            text: "Please correct username and password",
+            duration: 3000,
+            destination: "https://github.com/apvarun/toastify-js",
+            newWindow: true,
+            close: true,
+            gravity: "top", // `top` or `bottom`
+            position: "right", // `left`, `center` or `right`
+            stopOnFocus: true, // Prevents dismissing of toast on hover
+            style: {
+              background: "linear-gradient(to right, #f02e0c, #f76f4d)",
+            },
+            onClick: function(){} // Callback after click
+          }).showToast();        setValue("")
+        setValue2("")
+    }
+}
+ 
     return (
         <div className='flex justify-center mt-10'>
             <div className=' p-2 rounded shadow-xl bg-white'>
                 <form action="" id='main' onSubmit={mainFull}>
                     <div>
-                        <input className='border mt-3 w-100 p-1 rounded' type="email" placeholder='Username' required />
+                        <input className='border hover:border-green-300 mt-3 w-100 p-1 rounded' value={value} onChange={(e)=>setValue(e.target.value)} type="email" placeholder='Username' required />
                     </div>
                     <div>
-                        <input className='border mt-3 w-100 p-1 rounded' type="password" placeholder='Password' required />
+                        <input className='border  hover:border-green-300  mt-3 w-100 p-1 rounded' value={value2}  onChange={(e)=>setValue2(e.target.value)} type="password" placeholder='Password' required />
                     </div>
                     <div>
-                        <button className='p-1 bg-green-400 font-semibold  rounded w-100 mt-3'>Sumbit</button>
+                        <button onClick={sumbitButton} className='p-1 bg-green-400 font-semibold  rounded w-100 mt-3'>Sumbit</button>
                     </div>
                 </form>
             </div>
