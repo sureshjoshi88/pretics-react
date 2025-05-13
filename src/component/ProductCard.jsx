e5097af3e7a383aaabc1d0c714f7cc0772d4f6c4
sureshjoshi88 <sj3795789@gmail.com>
2025-05-13 16:39:36 +0530

    updated code

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -27,23 +27,10 @@ const ProductCard = (props) => {
   
 
   const [addcard,setAddcard] = useState([]);
+  const [cardcount,setCardcount] = useState(0);
 
-  const cardbuton=(event)=>{
-  let imges = event.target.parentElement.parentElement.firstChild.src;
-  let ides = event.target.parentElement.parentElement.childNodes[1].innerText;
-  let names = event.target.parentElement.parentElement.childNodes[2].innerText;
-  let prices = event.target.parentElement.parentElement.childNodes[3].innerText;
-    let data={
-      img:imges,
-      id:ides,
-      name:names,
-      price:prices
 
-    }
-    setAddcard(pre=>[...pre,data]);
-    console.log(addcard);
- 
-  }
+
   return (
     <>
 
@@ -51,7 +38,7 @@ const ProductCard = (props) => {
            <h2 className='font-bold text-3xl text-center'>My Social Feed</h2>
 
       <div className='flex flex-wrap justify-around  '>
-       <button id="card" onClick={slider} className=' text-3xl text-black rounded  p-1 cursor-pointer relative'><IoCartSharp /><span style={{borderRadius:"50%",height:"24px",width:"24px",fontSize:"16px"}} className='absolute -top-4.5  -right-2 bg-red-500 font-semibold rounded-b-full text-white'>{1}</span>
+       <button id="card" onClick={slider} className=' text-3xl text-black rounded  p-1 cursor-pointer relative'><IoCartSharp /><span style={{borderRadius:"50%",height:"24px",width:"24px",fontSize:"16px"}} className='absolute -top-4.5  -right-2 bg-red-500 font-semibold rounded-b-full text-white'>{cardcount}</span>
 </button>
        <select  onChange={(e)=>setSalected(e.target.value)} name="" id="salect" className='border mt-2'>
             <option value="all">all</option>
@@ -63,19 +50,18 @@ const ProductCard = (props) => {
     </div>
   </div>
 
-   <div className='sticky top-10'>
-     <div id='slider-bar' className='h-130 w-100 absolute  bg-white overflow-y-scroll flow-cart rounded -left-100'>
+   <div className='sticky top-22'>
+     <div id='slider-bar' className='md:h-130 h-70 md:w-100 w-90 absolute  bg-white overflow-y-scroll flow-cart rounded -left-100'>
     <p onClick={removeSlider} className='text-4xl text-end pe-3 cursor-pointer'>x</p>
-
-    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
+          {addcard.length>0?"":<p className='text-center top-20'>No data found</p>}
     {addcard.map((items)=>{
-      return <div className='flex p-2 gap-2 items-center'>
+      return <div className='flex p-2 gap-3 items-center'>
         <img className='w-40 rounded ' src={items.img} alt="" />
         <p className='font-semibold '>{items.name}</p>
         <p className='font-semibold '>{items.price}</p>
-        
       </div>
     })}
+   
     </div>
    </div>
      <div className=' grid md:grid-cols-3 gap-4 mt-5 w-full  p-2'>
@@ -87,7 +73,7 @@ const ProductCard = (props) => {
         <p>Name: {value.name}</p>
         <p>Price: {value.price}</p>
         <div className='flex gap-3 flex-wrap'>
-                  <button onClick={cardbuton} id='card-buton' className='text-white rounded bg-red-400 h-8 w-30 p-1 mt-3 cursor-pointer'>Add to Cart</button>
+                  <button onClick={()=>addcard.includes(value)?setAddcard([...addcard]):setAddcard([...addcard,value])[ setCardcount(addcard.length+1)]} id='card-buton' className='text-white rounded bg-red-400 h-8 w-30 p-1 mt-3 cursor-pointer'>Add to Cart</button>
                   <button id='card-buton' className='text-white rounded bg-green-600 h-8 w-30 p-1 mt-3 cursor-pointer'>Buy Now</button>
         </div>
         <div className='mt-3'>
@@ -97,7 +83,6 @@ const ProductCard = (props) => {
       </div>
       })}
      </div>
-    
      </>
    
   )
