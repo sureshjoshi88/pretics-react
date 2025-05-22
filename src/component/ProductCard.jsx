aa9ba3af20e79db45a7449a8a02ef2457fa99543
sureshjoshi88 <sj3795789@gmail.com>
2025-05-22 21:52:46 +0530

    updated code

--- src/component/ProductCard.jsx
+++ src/component/ProductCard.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import { products } from "../datas/products"
 import Comment from './Comment';
-import { IoCartSharp } from "react-icons/io5";
+import { IoaddcardSharp } from "react-icons/io5";
 import { MdDeleteForever } from "react-icons/md";
 import { CiDark } from "react-icons/ci";
 import { MdSunny } from "react-icons/md";
@@ -56,19 +56,25 @@ const ProductCard = (props) => {
   }
 
 
-  const [addcard, setAddcard] = useState([]);
-  const [cardcount, setCardcount] = useState(0);
+  const [addcard, setAddcard] = useState(()=>{
+      return JSON.parse(localStorage.getItem("addcard")) || [];
+
+  });
+  const [cardcount, setCardcount] = useState(()=>{
+      return JSON.parse(localStorage.getItem("length")) || 0;
+
+  });
 
 
 
   const removeitem = (id) => {
-    const updatedCart = cart.filter(item => item.id !== id);
-    let totalproduct =  localStorage.setItem("cart",JSON.stringify(updatedCart));
-    let totalLength = localStorage.setItem("length",JSON.stringify(updatedCart.length))
-    setAddcard(updatedCart);
+    const updatedaddcard = addcard.filter(item => item.id !== id);
+    let totalproduct =  localStorage.setItem("addcard",JSON.stringify(updatedaddcard));
+    let totalLength = localStorage.setItem("length",JSON.stringify(updatedaddcard.length))
+    setAddcard(updatedaddcard);
     setCardcount(totalLength);
-
-
+     totalLength = updatedaddcard.length;
+    setCardcount(totalLength);
     Toastify({
       text: `product is succedfully delete`,
       duration: 3000,
@@ -87,30 +93,33 @@ const ProductCard = (props) => {
   }
 
 
-  let cart = JSON.parse(localStorage.getItem("cart"))||[];
-let length = JSON.parse(localStorage.getItem("length"))||0
-  const totalPrice = cart.reduce((acc, item) => acc + parseFloat(item.price.replace(/,/g, "")) * item.quantity, 0);
+
+useEffect(() => {
+  localStorage.setItem("cart", JSON.stringify(addcard));
+  localStorage.setItem("length", JSON.stringify(addcard.length));
+}, [addcard]);
+  const totalPrice = addcard.reduce((acc, item) => acc + parseFloat(item.price.replace(/,/g, "")) * item.quantity, 0);
 
 const formateTotal = totalPrice.toLocaleString("en-IN");
 
   const incrementQuantity = (id) => {
-    const updatedCart = addcard.map(item => {
+    const updatedaddcard = addcard.map(item => {
       if (item.id === id) {
         return { ...item, quantity: item.quantity + 1 };
       }
       return item;
     });
-    setAddcard(updatedCart);
+    setAddcard(updatedaddcard);
   };
 
   const decrementQuantity = (id) => {
-    const updatedCart = addcard.map(item => {
+    const updatedaddcard = addcard.map(item => {
       if (item.id === id && item.quantity > 1) {
         return { ...item, quantity: item.quantity - 1 };
       }
       return item;
     });
-    setAddcard(updatedCart);
+    setAddcard(updatedaddcard);
   };
 
 
@@ -124,7 +133,7 @@ const formateTotal = totalPrice.toLocaleString("en-IN");
         <h2 className='font-bold md:text-3xl text-center'>My Social Feed</h2>
 
         <div className='flex flex-wrap justify-around  '>
-          <button id="card" onClick={slider} className={`text-3xl ${mode === "light" ? 'text-black' : "text-white"} rounded  p-1 cursor-pointer relative`}><IoCartSharp /><span style={{ borderRadius: "50%", height: "24px", width: "24px", fontSize: "16px" }} className='absolute -top-4.5  -right-2 bg-red-500 font-semibold rounded-b-full text-white'>{length}</span>
+          <button id="card" onClick={slider} className={`text-3xl ${mode === "light" ? 'text-black' : "text-white"} rounded  p-1 cursor-pointer relative`}><IoaddcardSharp /><span style={{ borderRadius: "50%", height: "24px", width: "24px", fontSize: "16px" }} className='absolute -top-4.5  -right-2 bg-red-500 font-semibold rounded-b-full text-white'>{cardcount}</span>
           </button>
           <select onChange={(e) => setSalected(e.target.value)} name="" id="salect" className={`${mode === "light" ? "text-black " : "text-white"} border rounded mt-2`}>
             <option className={`${mode === "light" ? "text-black " : "text-white bg-black"}`} value="all">all</option>
@@ -143,12 +152,12 @@ const formateTotal = totalPrice.toLocaleString("en-IN");
       </div>
 
       <div className='sticky top-22 '>
-        <div id='slider-bar' className={`md:h-130 h-70 md:w-100 w-90 absolute   overflow-y-scroll flow-cart rounded -left-100  ${mode === "light" ? "text-black bg-white" : "text-white bg-black"}`}>
+        <div id='slider-bar' className={`md:h-130 h-70 md:w-100 w-90 absolute   overflow-y-scroll flow-addcard rounded -left-100  ${mode === "light" ? "text-black bg-white" : "text-white bg-black"}`}>
           <p onClick={removeSlider} className='text-4xl text-end pe-3 cursor-pointer sticky top-2.5'>x</p>
-          {cart.length > 0 && <p className='text-center font-bold text-xl p-2'>Total: ₹ {formateTotal}</p>}
-          {cart.length > 0 ? "" : <p className='text-center top-20'>No data found</p>}
+          {addcard.length > 0 && <p className='text-center font-bold text-xl p-2'>Total: ₹ {formateTotal}</p>}
+          {addcard.length > 0 ? "" : <p className='text-center top-20'>No data found</p>}
           {
-          cart.map((items,index) => {
+          addcard.map((items,index) => {
             return <div key={index}>
               <div className='flex p-2 gap-3 items-center'>
                 <img className='w-40 h-50 object-contain rounded ' src={items.img} alt="" />
@@ -183,12 +192,12 @@ const formateTotal = totalPrice.toLocaleString("en-IN");
               <p className='ps-4 font-semibold'>Price: {value.price}</p>
               <div className='flex gap-3 flex-wrap'>
                 <button onClick={() => {
-                  if (!cart.find((item) => item.id === value.id)) {
-                    const newcart = [...cart, { ...value, quantity: 1 }];
-                    setAddcard(newcart);
-                    let totalproduct =  localStorage.setItem("cart",JSON.stringify(newcart));
-                    setCardcount(newcart.length);
-                    let totalLength = localStorage.setItem("length",JSON.stringify(newcart.length))
+                  if (!addcard.find((item) => item.id === value.id)) {
+                    const newaddcard = [...addcard, { ...value, quantity: 1 }];
+                    setAddcard(newaddcard);
+                    let totalproduct =  localStorage.setItem("cart",JSON.stringify(newaddcard));
+                    setCardcount(newaddcard.length);
+                    let totalLength = localStorage.setItem("length",JSON.stringify(newaddcard.length))
                     Toastify({
                       text: `product is succedfully added `,
                       duration: 3000,
@@ -219,7 +228,7 @@ const formateTotal = totalPrice.toLocaleString("en-IN");
                       onClick: function () { } // Callback after click
                     }).showToast();
                   }
-                }} id='card-buton' className='text-white rounded bg-red-400 h-8 w-30 p-1 mt-3 cursor-pointer'>Add to Cart</button>
+                }} id='card-buton' className='text-white rounded bg-red-400 h-8 w-30 p-1 mt-3 cursor-pointer'>Add to addcard</button>
 
                 <button id='card-buton' className='text-white rounded bg-green-600 h-8 w-30 p-1 mt-3 cursor-pointer'>Buy Now</button>
               </div>
