7f75e028681885cf7dd31e7fb063a429d4514ded
sureshjoshi88 <sj3795789@gmail.com>
2025-05-29 16:19:52 +0530

    upadted

--- src/datas/products.js
+++ src/datas/products.js
@@ -1,11 +1,110 @@
 export const products = [
-    { id: 1, name: "laptop", price: "50,000", img: "https://img.freepik.com/free-photo/still-life-books-versus-technology_23-2150062920.jpg?semt=ais_hybrid&w=740", catergory: "electric" },
-    { id: 2, name: "mobile", price: "20,000", img: "https://i.pinimg.com/736x/06/ac/af/06acaf69d781f5f13518bc8e084258c5.jpg", catergory: "electric" },
-    { id: 3, name: "Headphones", price: "10,000", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlwxosWqpw8mUj97usc80HchSbruOguOJc0g&s", catergory: "electric" },
-    { id: 4, name: "scorpio", price: "24,00,000", img: "https://imgd.aeplcdn.com/1280x720/n/cw/ec/128413/scorpio-exterior-right-front-three-quarter-47.jpeg?isig=0&q=80", catergory: "vihicle" },
-    { id: 5, name: "creata", price: "16,00,000", img: "https://imgd.aeplcdn.com/664x374/n/cw/ec/141115/creta-exterior-right-front-three-quarter-16.jpeg?isig=0&q=80", catergory: "vihicle" },
-    { id: 6, name: "honda", price: "18,00,000", img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSga0e2jecxSRGxHXkj3IgfKWoQ8YtsVPubfQ&s", catergory: "vihicle" },
-    { id: 7, name: "T-shirt", price: "1,200", img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQkBXfnvx4fswAhpNqL8NP69fP4D-1fTsFddvLy9V-TerE0I_hNzWZnBR4iCwVCJJHNYC34P-pFx_jY4es0ABRRoprwgF7L1w9TFOjZ9LRv3wucXrVm_07fxWhb", catergory: "clothes" },
-    { id: 8, name: "shirt", price: "2,000", img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTzqacKyWH8le16puxVSL1ZJZqlYCDVyYhA-fjzUt9yyyXRokTN461c_XQBd5-o9Mi0RpWe8QHQYQxye4_heunF2fNf2q6FXIYYUX4VCXxC", catergory: "clothes" },
-    { id: 9, name: "shirt-black", price: "2,600", img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRQ5mCxcNKb1dxH4sVmZmz1eBCAsg7qup2KDq52RvcNBMnzgxOX6bc3E72jQfiGRvYW8GtxuPbKqdvHCPWiX_i7iY5cBOcb2Iimp_hcUV3j", catergory: "clothes" },
+    {
+        id: 1,
+        name: "laptop",
+        price: "50,000",
+        img: "https://img.freepik.com/free-photo/still-life-books-versus-technology_23-2150062920.jpg?semt=ais_hybrid&w=740",
+        catergory: "electric"
+    },
+    {
+        id: 2, name: "mobile",
+        price: "20,000",
+        img: "https://i.pinimg.com/736x/06/ac/af/06acaf69d781f5f13518bc8e084258c5.jpg",
+        catergory: "electric"
+
+    },
+    {
+        id: 3,
+        name: "Headphones",
+        price: "10,000",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQlwxosWqpw8mUj97usc80HchSbruOguOJc0g&s",
+        catergory: "electric"
+    },
+    {
+        id: 4,
+        name: "scorpio",
+        price: "24,00,000",
+        img: "https://imgd.aeplcdn.com/1280x720/n/cw/ec/128413/scorpio-exterior-right-front-three-quarter-47.jpeg?isig=0&q=80",
+        catergory: "vihicle"
+    },
+    {
+        id: 5,
+        name: "creata",
+        price: "16,00,000",
+        img: "https://imgd.aeplcdn.com/664x374/n/cw/ec/141115/creta-exterior-right-front-three-quarter-16.jpeg?isig=0&q=80",
+        catergory: "vihicle"
+    },
+    {
+        id: 6,
+        name: "honda",
+        price: "18,00,000",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSga0e2jecxSRGxHXkj3IgfKWoQ8YtsVPubfQ&s",
+        catergory: "vihicle"
+    },
+    {
+        id: 7,
+        name: "T-shirt",
+        price: "1,200",
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcQkBXfnvx4fswAhpNqL8NP69fP4D-1fTsFddvLy9V-TerE0I_hNzWZnBR4iCwVCJJHNYC34P-pFx_jY4es0ABRRoprwgF7L1w9TFOjZ9LRv3wucXrVm_07fxWhb",
+        catergory: "clothes"
+
+
+
+    },
+    {
+        id: 8,
+        name: "shirt",
+        price: "2,000",
+        img: "https://encrypted-tbn3.gstatic.com/shopping?q=tbn:ANd9GcTzqacKyWH8le16puxVSL1ZJZqlYCDVyYhA-fjzUt9yyyXRokTN461c_XQBd5-o9Mi0RpWe8QHQYQxye4_heunF2fNf2q6FXIYYUX4VCXxC",
+        catergory: "clothes"
+    },
+    {
+        id: 9,
+        name: "shirt-black",
+        price: "2,600",
+        img: "https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcRQ5mCxcNKb1dxH4sVmZmz1eBCAsg7qup2KDq52RvcNBMnzgxOX6bc3E72jQfiGRvYW8GtxuPbKqdvHCPWiX_i7iY5cBOcb2Iimp_hcUV3j",
+        catergory: "clothes"
+    },
+    {
+        id: 12,
+        name: "Rich Dad Poor Dad",
+        price: "₹450",
+        img: "https://m.media-amazon.com/images/I/81BE7eeKzAL.jpg",
+        category: "books",
+    },
+    {
+        id: 13,
+        name: "Atomic Habits",
+        price: "₹500",
+        img: "https://images.unsplash.com/photo-1524995997946-a1c2e315a42f",
+        category: "books",
+    },
+    {
+        id: 14,
+        name: "Remote Car",
+        price: "₹1,500",
+        img: "https://m.media-amazon.com/images/I/5156vdTtaRL._SY300_SX300_QL70_FMwebp_.jpg",
+        category: "toys",
+    },
+    {
+        id: 15,
+        name: "Lego Blocks",
+        price: "₹1,800",
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbGf-37upr73XtAEGBkk7g2Amu5w-8vhZiRw&s",
+        category: "toys",
+    }, {
+        id: 10,
+        name: "Wooden Table",
+        price: "₹5,500",
+        img: "https://images.unsplash.com/photo-1519710164239-da123dc03ef4",
+        category: "furniture",
+    },
+    {
+        id: 11,
+        name: "Office Chair",
+        price: "₹3,200",
+        img: "https://thesleepcompany.in/cdn/shop/files/1_86ee36eb-042d-4d7d-aafc-c505fcc204e2.webp?v=1747593557&width=860",
+        category: "furniture",
+    }
+
 ]
