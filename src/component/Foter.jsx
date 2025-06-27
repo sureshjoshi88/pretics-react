475cdd4891387f99d620b6d96cb04e670852ad8c
sureshjoshi88 <sj3795789@gmail.com>
2025-06-27 11:29:37 +0530

    set of loader fething api data

--- src/component/Foter.jsx
+++ src/component/Foter.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Foter = () => {
   return (
     <div className='p-1' >
-      <div className='grid md:grid-cols-2'>
+      <div className='flex justify-between items-center flex-wrap'>
         <div className='p-2 font-semibold font-serif'>Made by </div>
         <div className='p-2 font-semibold font-serif text-end'>Suresh joshi</div>
       </div>
